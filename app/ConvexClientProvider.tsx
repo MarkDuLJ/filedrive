93012bf1184cb3d9c522ee39c6c0fa9e8331e175
Mark Du <dulongjiebackup@gmail.com>
2024-03-20 16:51:12 -0400

    add clerk, middleware

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -2,6 +2,8 @@
 
 import { ReactNode } from "react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
@@ -12,8 +14,10 @@ export default function ConvexClientProvider({
 }){
 
     return (
-        <ConvexProvider client={convex}>
-            {children}
-        </ConvexProvider>
+        <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
+            <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+                {children}
+            </ConvexProviderWithClerk>
+        </ClerkProvider>
     )
 }
---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+
+export default {
+    providers: [
+      {
+        domain: "https://possible-sunfish-80.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
